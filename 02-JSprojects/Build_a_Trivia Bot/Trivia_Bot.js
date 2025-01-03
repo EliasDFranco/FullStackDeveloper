74c630b1cf350c78011e6f0575543b200d09148e
EliasDFranco <eliasfranco20160055@gmail.com>
2025-01-03 17:37:49 -0300

    Adding bot trivia

---
+++ 02-JSprojects/Build_a_Trivia Bot/Trivia_Bot.js
@@ -0,0 +1,25 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+let botName;
+let botLocation;
+let favoriteLanguage;
+
+botName = "Elias";
+botLocation = "Asunción";
+favoriteLanguage = "C++";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+let codingFact = "My favorite programming language is " + favoriteLanguage + ".";
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+console.log(codingFact);
+
+codingFact = "I like to program in " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "Elias love " + favoriteLanguage + " because of its efficiency.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
