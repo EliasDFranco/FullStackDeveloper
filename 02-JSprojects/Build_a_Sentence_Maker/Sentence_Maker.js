303dccea5accd560aa93da6f66914b6fee443098
EliasDFranco <eliasfranco20160055@gmail.com>
2025-01-06 16:22:38 -0300

    Adding sentence maker

---
+++ 02-JSprojects/Build_a_Sentence_Maker/Sentence_Maker.js
@@ -0,0 +1,29 @@
+let adjective;
+let noun;
+let verb;
+let place; 
+let adjective2;
+let noun2;
+
+adjective = "Happy";
+noun = "Dog";
+verb = "Work";
+place = "Hotel Budapest";
+adjective2 = "Angry";
+noun2 = "Garden";
+
+const firstStory = "Once upon a time, there was a(n) " +  adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+let secondStory;
+adjective = "Beautiful";
+noun = "Dragon";
+verb = "Jump";
+place = "Gotham City"; 
+adjective2 = "Quick";
+noun2 = "River";
+
+secondStory = "Once upon a time, there was a(n) " +  adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
